35ac12bc0a2158b0c443cf8c1c5b1797748f5823
Saliazar17 <jsiliezarmoreno123@gmail.com>
2024-11-02 01:07:39 -0600

    actualizacion de proyecto

--- data/global/nav.js
+++
@@ -1,8 +0,0 @@
-export default {
-    "nav" : [
-        {label: 'Inicio', uri: 'index.html'},
-        {label: 'Galería', uri: 'gallery.html'},
-        {label: 'Catálogo', uri: 'product.html'},
-        {label: 'Promoción', uri: 'productos/producto1.html'},
-    ]
-}
