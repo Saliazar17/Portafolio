7ec642b9ba1ee5f39c5a761cdd008c8d0956b29a
Saliazar17 <jsiliezarmoreno123@gmail.com>
2024-11-02 12:36:46 -0600

    actualizacion de nav

--- data/index.js
+++ data/index.js
@@ -1,16 +1,8 @@
 import { getHeroData } from "./hero/hero";
 
-//----------------Nav
-import navData from "./global/nav";
-//--------------- Productos 
-
-
-// tablas
-
 export const generarContextoDePagina = (page)=>{
     console.log('page', page);
     const contextObject = {
-        ...navData,
     };
     let pageObject = {}
     switch(page){
@@ -19,9 +11,6 @@ export const generarContextoDePagina = (page)=>{
             break;
         case '/index.html':
             pageObject = getHeroData();
-            break;
-        case '/productos/producto1.html':
-            pageObject = getProductData();
             break;
         default:
             pageObject = {};
